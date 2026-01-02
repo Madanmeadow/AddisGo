3bf6fdc5e918b3e4c7182a2abbd51e93f552276f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-01-02 01:34:27 -0600

    Fix Contact route import

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router"
 
+// ✅ IMPORT ALL PAGES (THIS WAS MISSING)
 import Home from "../pages/Home.vue"
+import About from "../pages/About.vue"
 import Services from "../pages/Services.vue"
 import Contact from "../pages/Contact.vue"
+import Drivers from "../pages/Drivers.vue"
+import Hotels from "../pages/Hotels.vue"
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
   },
   {
     path: "/services",
@@ -20,6 +29,16 @@ const routes = [
     name: "Contact",
     component: Contact,
   },
+  {
+    path: "/drivers",
+    name: "Drivers",
+    component: Drivers,
+  },
+  {
+    path: "/hotels",
+    name: "Hotels",
+    component: Hotels,
+  },
 ]
 
 const router = createRouter({
@@ -28,3 +47,4 @@ const router = createRouter({
 })
 
 export default router
+r
