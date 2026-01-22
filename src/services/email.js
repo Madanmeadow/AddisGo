7920fb1ae057a47151321dc469a406dc8a741286
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-01-22 02:05:02 -0600

    Fix EmailJS contact form

--- src/services/email.js
+++ src/services/email.js
@@ -5,9 +5,9 @@ export function sendContactEmail({ name, email, message}) {
     'service_wn78sgc',        // ✅ your Service ID
     'template_gxz5kzl',       // ✅ your Template ID
     {
-      from_name: formData.name,
-      reply_to: formData.email,
-      message: formData.message,
+      from_name: name,
+      reply_to: email,
+      message: message,
     },
     'tDY5BR8IN9QpXqeBM'       // ✅ your PUBLIC KEY ONLY
   );
