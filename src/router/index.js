e1edaffbd78e4d3f7b7eb694d24b2b31e9883ff4
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-01-25 23:17:01 -0600

    ok boo

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from '@/pages/Home.vue';
-import About from '@/pages/About.vue';
-import Contact from '@/pages/Contact.vue';
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+import Contact from '../pages/Contact.vue';
+import Services from '../pages/Services.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
-  { path: '/contact', component: Contact },
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '/contact', component: Contact },
+    { path: '/services', component: Services },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
