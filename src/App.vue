01fba113c31dd1e1dfa29cf86f3da8d0650e40ba
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-01-28 23:54:51 -0600

    hu

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
-<template>
-  <router-view />
-</template>
+import api from "@/services/api";
+
+export default {
+  name: "TestAPI",
+  mounted() {
+    api.get("/api/auth/test")
+      .then(res => {
+        console.log("API RESPONSE:", res.data);
+      })
+      .catch(err => {
+        console.error("API ERROR:", err.message);
+      });
+  }
+};
