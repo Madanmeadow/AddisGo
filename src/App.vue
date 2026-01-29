b489d77006d8f3249df188cd99245a1c9410509d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-01-29 00:46:31 -0600

    yeees

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
+<script>
 import api from "@/services/api";
 
 export default {
-  name: "TestAPI",
+  name: "App",
   mounted() {
     api.get("/api/auth/test")
       .then(res => {
         console.log("API RESPONSE:", res.data);
       })
       .catch(err => {
-        console.error("API ERROR:", err.message);
+        console.error("API ERROR:", err);
       });
   }
 };
+</script>
+
+<template>
+  <h1>Frontend is running</h1>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default {
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+};
