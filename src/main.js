149d7f23d9aba6e9aba6b32030ca1ab92f603b9d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-02-04 23:41:42 -0600

    hihi

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { registerSW } from "virtual:pwa-register";
 
-createApp(App)
-  .use(router)
-  .mount("#app");
+registerSW({ immediate: true });
+
+createApp(App).use(router).mount("#app");
