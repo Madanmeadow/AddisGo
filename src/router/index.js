a04e5eec73c8733ab8b61119e7f937d9723eedaf
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-02-05 01:04:17 -0600

    ushh

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,38 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+// Views
 import HomeView from "@/views/HomeView.vue";
 import ExploreView from "@/views/ExploreView.vue";
+import UploadView from "@/views/UploadView.vue";
 
 const routes = [
-  { path: "/", name: "Home", component: HomeView },
-  { path: "/explore", name: "Explore", component: ExploreView }
+  {
+    path: "/",
+    name: "Home",
+    component: HomeView
+  },
+  {
+    path: "/explore",
+    name: "Explore",
+    component: ExploreView
+  },
+  {
+    path: "/upload",
+    name: "Upload",
+    component: UploadView
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/"
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior() {
+    return { top: 0 };
+  }
 });
 
 export default router;
-
