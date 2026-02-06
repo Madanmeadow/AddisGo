db5afa4cc6b140a46d86a2e68a72ff73be5c8f50
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-02-06 16:38:48 -0600

    hey

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
-import { io } from "socket.io-client";
+// src/services/socket.js
 
-const socket = io("http://localhost:5000", {
-  withCredentials: true
-});
-
-export default socket;
+export default {
+  on() {},
+  emit() {},
+  disconnect() {}
+};
