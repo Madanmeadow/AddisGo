338ff5ee7ac9b1de980fcfea570dbc8c5618ab81
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-02-09 01:51:51 -0600

    gh

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 import dotenv from "dotenv";
-dotenv.config();
-
 import app from "./app.js";
 
-const PORT = process.env.PORT;
+dotenv.config();
 
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 AddisGo API running on port ${PORT}`);
 });
