8a1f6eb300fb359ecbb5cf0bb88ba9ae9962d82c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-02-09 01:29:04 -0600

    okj

--- server/index.js
+++ server/index.js
@@ -5,10 +5,7 @@ import app from "./app.js";
 
 const PORT = process.env.PORT;
 
-if (!PORT) {
-  console.error("❌ PORT is not defined");
-  process.exit(1);
-}
+
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
