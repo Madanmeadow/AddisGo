0c4706f6b760a752b94451e05be20b4714ef7a9e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-03-05 01:00:19 -0600

    gh

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -42,7 +42,7 @@ async function login() {
 
 <template>
   <div class="auth">
-    <h1>🔥 AddisGo Login</h1>
+    <h1>🔥 pulse Login</h1>
 
     <input v-model="email" placeholder="Email" />
     <input v-model="password" type="password" placeholder="Password" />
