683c52df298229df3f05ba1f39aca000bd6bb427
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-03-26 00:46:14 -0500

    jk

--- server/mediasoup/workers.js
+++ server/mediasoup/workers.js
@@ -1,20 +1,12 @@
 // server/mediasoup/workers.js
+
 import * as mediasoup from "mediasoup";
 
-let worker;
-
 export async function initMediasoupWorker() {
-  worker = await mediasoup.createWorker({
+  global.mediasoupWorker = await mediasoup.createWorker({
     rtcMinPort: 20000,
     rtcMaxPort: 20200,
   });
-
-  worker.on("died", () => {
-    console.error("❌ Mediasoup worker died");
-    process.exit(1);
-  });
-
-  global.mediasoupWorker = worker;
 
   console.log("✅ Mediasoup Worker Ready");
 }
